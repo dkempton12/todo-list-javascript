27aeb8a169bbad9837280dbb2c6a8b893113960c
Dan Kempton <kemptondan@gmail.com>
2019-11-04 18:13:57 -0700

    Add addTodo method for adding todo items

--- js/app.js
+++ js/app.js
@@ -4,4 +4,12 @@ var todoList = {
   displayTodos: function() {
     console.log('My To-Dos:', this.todos);
   },
+
+  addTodo: function(todoText) {
+    this.todos.push({
+      todoText: todoText,
+      completed: false
+    });
+    this.displayTodos();
+  }
 };
